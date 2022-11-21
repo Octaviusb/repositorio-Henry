627f2a475ae9e9e3e4a73f852842bfa7556fb849
Ener Octavio Buitrago Camelo <obuitrago camelo@yahoo.es>
2022-11-21 18:25:23 -0500

    nuevo intento

---
+++ 02-JS-I/homework/text.js
@@ -0,0 +1,17 @@
+1.	//En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+// Respuestas:
+
+//•	Variables: en lenguaje Js, una variable se puede definir como un elemento o un objeto (cualquier cosa) que es como una caja que puede contener otra cosa u otro elemento Ej. Podemos guardar un objeto que hace algo, dentro de una caja “variable”
+
+//•	Strings: En cualquier lenguaje de programación, un string es una secuencia de caracteres que se usa para representar el texto Ej. Un string es una cadena secuencial de las letras del alfabeto o los números naturales
+
+//•	Funciones (argumentos, return) una función es un conjunto de instrucciones que realiza una tarea o calcula un valor, pero para que un procedimiento califique como una función, debe tomar alguna entrada y devolver una salida.
+
+//•	Arguments: es un objeto que se encuentra dentro de funciones y contiene los valores de los argumentos pasados a esa función.
+
+//•	Return es una sentencia que finaliza la ejecución de la función y especifica un valor para ser devuelto a quien llama a la función.
+
+//•	Declaraciones if: Es un condicional que sirve para realizar unas u otras operaciones en función de una expresión. Ej evalúa la epresión, si es positivo, dará resultado = true; en caso contrario el resultado será = false
+
+//•	Valores booleanos (true, false) son expresiones que arrojan dos resultados: true (verdadero) o (falso) false
