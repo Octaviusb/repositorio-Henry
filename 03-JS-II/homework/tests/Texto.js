4427f03b45fde424efd01e19545929eda77b8c46
Ener Octavio Buitrago Camelo <obuitrago camelo@yahoo.es>
2022-11-23 11:42:31 -0500

    'Actualizacion'

---
+++ 03-JS-II/homework/tests/Texto.js
@@ -0,0 +1,20 @@
+En un archivo de texto separado que debes crear, escribe explicaciones de 
+los siguientes conceptos como si se lo estuvieras explicando a un niño de 12
+años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu 
+comprensión.
+for
+&&, ||, !
+
+for: En Javascript, "for" crea un bucle que consiste en tres expresiones encerradas en 
+paréntesis y separadas por puntos y comas, seguidas de una sentencia ejecutada 
+en un bucle. Ej: for (num1, num2, num3); esta sería la expresion, pero tambien requiere un
+elemento condicional que le dice que debe hacer y tamien una sentencia de hasta donde o
+hasta cuando.
+
+&& representan a "Y" en español o and en ingles y se usa literanmente como una conjuncion
+para incluir un elemento dentro de la misma condición. Ej. (ver i >= 1 && == 4).Ej
+
+|| este simbolo se refiere a la letra "O" y "or" en ingles, yen el caso de una comparación
+se usa para hacer una seleccion.
+
+! Este simbolo representa una negación Ej. i != j se lee i no es igual a j.
