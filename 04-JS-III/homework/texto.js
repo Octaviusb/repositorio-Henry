8507c63ae52ab5f6122d2e71cf14d32946d9d7c8
Ener Octavio Buitrago Camelo <49449065+Octaviusb@users.noreply.github.com>
2022-11-28 13:20:18 -0500

    Add files via upload

---
+++ 04-JS-III/homework/texto.js
@@ -0,0 +1,13 @@
+1.	En un archivo de texto separado que debes crear, escribe 
+explicaciones de los siguientes conceptos como si se lo estuvieras 
+explicando a un niño de 12 años. Hacer esto te ayudará a descubrir 
+rápidamente cualquier agujero en tu comprensión.
+o	Arrays
+
+Array, traduce arreglo, es un “objeto” de javascript que se compone
+de una lista de otros elementos contenidos dentro de ese objeto.
+Array `permite modificar, ordenar, reordenar , listar en orden de 
+principio a fin o al revés, admite, expresiones, strings, funciones, 
+variables o booleanos entre otras muchas propiedades.
+Se dice que array, permite que los objetos listados muten lo cual 
+implica que puedan  modificarse o invocar individualmente
